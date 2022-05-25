018d68e594308dcf1fa886e73b16f3aad04d1e18
Manuel Bueno <manbuegom@alum.us.es>
2022-05-25 18:49:54 +0200

    Minor Feature: Añadida una pantalla inicio (home)
    Se ha añadido una pantalla inicial de ayuda para usuarios en la ruta /

---
+++ src/components/Home.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+</script>
+
+<h1 class="text-center text-4xl text-gray-700 font-semibold mb-16">
+    Healthcare - Management System
+</h1>
+
+<div
+    class="max-w-2xl p-4 mx-auto mt-4 sm:bg-lime-200 sm:p-6 md:bg-lime-200"
+>
+
+    <br>
+    <h2 class="text-4xl font-bold text-left text-gray-700 sm:text-4xl">
+        Welcome!
+    </h2>
+    <br />
+    <h3 class="text-xl font-semibold text-left text-gray-700 sm:text-xl">
+        Quick start!
+        
+    </h3>
+
+    <p class="m-2 font-semibold mt-8">
+        Here you have a quick guide of each component of the application:
+    </p>
+    <p class="m-2 mt-8">
+        • <b>Patients:</b> Here you can look for a specific patient and check his/her details as well as observations.
+    </p>
+    <p class="m-2 mt-8">
+        • <b>New Patient/New Practitioner:</b> Both options manage patient/practitioner registration in the system.
+    </p>
+    <p class="m-2 mt-8">
+        • <b>Practitioners:</b> This section contains a list of practitioners in which you can search a specific practitioner and check her/his details.
+    </p>
+    <p class="m-2 mt-8">
+        • <b>New Pract. Role:</b> Here you can find a list of the registered roles and manage them.
+    </p>
+        
+</div>
